e33ce85b41713acde7e81dab65fdcd4460c50426
sukhrob22 <ergashevsuxrob986@gmail.com>
2023-12-08 10:46:43 +0500

    Layout

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 import express from 'express';
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { engine } from 'express-handlebars';
+import { engine, create } from 'express-handlebars';
+// import path, { dirname } from 'path';
+// import { fileURLToPath } from 'url';
 
 // bu yo'lar orqali biz dirnameni pathdan chiqarib olamiz bunday qilmasak dirname chiqmaydi bzini faylarimiz module struktufada bo'lganiligi uchun
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
 const app = express();
 
+const hbs = create({
+    // bu orqali biz handlebars qilib uzun yozmaymizda o'niga hbs qilib yozib qo'ysak bo'ladi main.handlebars ni o'rniga main.hbs
+    defaultLayout: 'main',
+    extname: 'hbs',
+});
+
 // bu 3 qator kod bilan o'zimizning divijogimizni yuklab oldik ya'ni handlebarsni
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
 app.set('views', './views');
 
 app.get('/', (req, res) => {
