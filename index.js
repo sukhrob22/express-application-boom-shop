b65c52ae556089d65276440bb00d7bcad29e6219
sukhrob22 <ergashevsuxrob986@gmail.com>
2023-12-11 10:28:35 +0500

    MongoDB ulash & .env

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import { engine, create } from 'express-handlebars';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+//Routes
 import AuthRoutes from './routes/auth.js';
 import PoductsRoutes from './routes/products.js';
+
+dotenv.config();
 // import path, { dirname } from 'path';
 // import { fileURLToPath } from 'url';
 
@@ -21,10 +26,12 @@ const hbs = create({
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', './views');
+
 // bu biz ochgan puplic static faylimizni boshqa faylarga bog'lashga yordam beradi
 app.use(express.static('public'));
 // biz ma'lumotni json yo o'zimzi hoxlagan objectga keltirmoqchi bo'lsak shunday foylanamiz
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // bu use desak qandaydir extra method obj yo func ishlatayotgan bo'lamiz, biz middlewarsni get so'rovdagi callbackfuncdan oldin ham qo'ysak bo'ladi
 //  keyun o'sha so'rovdan oldin ham qo'ysak bo'ladi use dek
@@ -32,5 +39,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(AuthRoutes);
 app.use(PoductsRoutes);
 
-const PORT = process.env.PORT || 4100;
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+// console.log(process.env.MONGO_URI);
+const startApp = () => {
+    try {
+        mongoose.set('strictQuery', false);
+        mongoose.connect(
+            process.env.MONGO_URI,
+            {
+                useNewUrlParser: true,
+            },
+            () => console.log('Mongo db conntected')
+        );
+
+        const PORT = process.env.PORT || 4100;
+        app.listen(PORT, () =>
+            console.log(`Server is running on port: ${PORT}`)
+        );
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+startApp();
